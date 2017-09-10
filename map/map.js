b438b4337898a88c628676a3ff6927aa2f63b327
olivernyc <oliver@static.network>
2017-09-09 21:52:09 -0400

    Add infoWindow

--- map/map.js
+++ map/map.js
@@ -1,4 +1,5 @@
 var map;
+var currentNode = {};
 
 function initMap() {
   var styles = [
@@ -157,7 +158,7 @@ function initMap() {
   //changing icon opacity to show it has panoramas, also set supernode icon
   activeNodesLayer.setStyle(function(feature) {
     var url = "../assets/images/active.svg";
-    var opacity = 0.5;
+    var opacity = 0.75;
     var notes = feature.getProperty("notes").toLowerCase();
     if (notes.indexOf("supernode") !== -1) {
       url = "../assets/images/supernode.svg";
@@ -232,59 +233,29 @@ function initMap() {
   });
 
   var infowindow = new google.maps.InfoWindow();
-  activeNodesLayer.addListener("click", openInfoWindow);
-  potentialNodesLayer.addListener("click", openInfoWindow);
+  activeNodesLayer.addListener("click", showDetails);
+  potentialNodesLayer.addListener("click", showDetails);
 
   linksLayer.setMap(map);
   linkNYCLayer.setMap(map);
   potentialNodesLayer.setMap(map);
   activeNodesLayer.setMap(map);
+}
 
-  function openInfoWindow(event) {
-    // replace this with a fully custom overlay
-    var content = "<div class='pv3'>";
-    content +=
-      "<h2 class='di pr2'>Node " +
-      event.feature.f.id +
-      "" +
-      event.feature.f.otherNodes +
-      "</h2>";
-
-    if (event.feature.f.status == "Installed") {
-      content +=
-        "<h3 class='di green fw4'>Active</h3><br>" + event.feature.f.notes;
-    } else {
-      content +=
-        "<h3 class='di gray fw4'>Potential</h3><br>" + event.feature.f.notes;
-    }
-
-    content += "</div>";
-
-    var panoramas = event.feature.f.panoramas;
-    if (panoramas) {
-      content +=
-        "<h4 class='pb2 mv0 mb3 near-black fw4'>View from this node:</h3>";
-      for (var i = 0; i < panoramas.length; i++) {
-        var image =
-          "<div class='w6'>" +
-          "<a href='" +
-          "../panorama/" +
-          panoramas[i] +
-          "'>";
-        image +=
-          "<img class='w-100 h-100 contain' src='" +
-          "../panorama/" +
-          panoramas[i] +
-          "'></img>";
-        image += "</a>" + "</div";
-
-        content += image;
-      }
-    }
-    infowindow.setContent(content);
-    infowindow.setPosition(event.feature.getGeometry().get());
-    infowindow.setOptions({ pixelOffset: new google.maps.Size(-1, -8) });
-    infowindow.open(map);
-    setTimeout(infowindow.open(map), 10000); // needs a timeout delay to force the autoscroll !!
+function showDetails(event) {
+  var node = event.feature.f
+  currentNode = node
+  var infoWindow = document.getElementById('infoWindow')
+  infoWindow.innerHTML = '<div class="flex items-center justify-between">'+'<h2 class="mv0">Node #'+node.id+'</h2>'+'<h3 class="mv0 '+node.status+'">'+node.status+'</h3>'+'</div>'
+  if (node.panoramas) {
+    String(node.panoramas).split(',').forEach(function(panorama) {
+      infoWindow.innerHTML += '<img class="w-100 mv2" src="/panorama/'+panorama+'"></img>'
+    })
   }
+  infoWindow.classList.add('db');
+}
+
+function hideDetails() {
+  var infoWindow = document.getElementById('infoWindow')
+  infoWindow.classList.remove('db');
 }
